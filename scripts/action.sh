00c105e2ed71ebd3399c10ea41bd72dfcd695cbf
noneill <noneill@patriotsoftware.com>
2024-11-04 13:41:23 -0500

    Debugging

--- scripts/action.sh
+++ scripts/action.sh
@@ -17,8 +17,8 @@ fi
 
 # Build the Docker image
 echo "*** Building Dockerfile ***"
-echo "Command: build -t "$DOCKER_FULL_IMAGE_NAME:$DOCKER_TAG" -f $DOCKERFILE_PATH $DOCKER_CONTEXT --no-cache $DOCKER_ARGS_LIST"
-docker build -t "$DOCKER_FULL_IMAGE_NAME:$DOCKER_TAG" -f $DOCKERFILE_PATH $DOCKER_CONTEXT --no-cache $DOCKER_ARGS_LIST
+echo "Command: build -t "$DOCKER_FULL_IMAGE_NAME:$DOCKER_TAG" -f $DOCKERFILE_PATH --no-cache $DOCKER_ARGS_LIST $DOCKER_CONTEXT"
+docker build -t "$DOCKER_FULL_IMAGE_NAME:$DOCKER_TAG" -f $DOCKERFILE_PATH --no-cache $DOCKER_ARGS_LIST $DOCKER_CONTEXT
 docker tag "$DOCKER_FULL_IMAGE_NAME:$DOCKER_TAG" "$DOCKER_FULL_IMAGE_NAME:latest"
 
 # Create the ECR Repository
